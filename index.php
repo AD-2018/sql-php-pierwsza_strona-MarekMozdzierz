85904b6e795058cd97a002bd0eb965133932a41f
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-11-05 08:36:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $sql= "SELECT * FROM pracownicy";
 echo("<h1>zadanie 1</h1>");
 echo("<li>".$sql."<br><br>");
 $result = mysqli_query($conn, $sql);
-echo('<table border="1" class="tabelka_moja">');
+echo('<table border="1" class="tabelkamoja">');
 while($row = mysqli_fetch_assoc($result) ) {
    echo('<tr>');
    echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
