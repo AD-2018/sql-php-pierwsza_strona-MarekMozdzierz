e8db2d4f342038e80773471d6f4076ead04c2316
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-11-13 11:52:29 +0100

    Update Pracownicyorganizacja.php

--- Pracownicyorganizacja.php
+++ Pracownicyorganizacja.php
@@ -1,228 +1,89 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MichaelMusiol">Github</a>
+    <h1>Marek Moździerz nr.21</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">github</a>
+    <br>
     <a href="index.php">Pracownicy</a>
-    <a href="Pracownicyorganizacja.php">Organizacja Pracownicy</a>
-    <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
-    <a href="DataiCzas.php">Data i Czas</a>
-    
-<?php
-require_once("lib.php");
-$sql ="select * from pracownicy,organizacja where id_org=dzial";
-echo("<h1>Pracownicy i Organizacja</h1>");
-echo("<h3>zad.1</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and (dzial=1 or dzial=4)";
-echo("<h3>zad.2</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and imie like '%a' ";
-echo("<h3>zad.3</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and imie not like '%a' ";
-echo("<h3>zad.4</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-echo("<h2>Sortowanie</h2>");
-    
-    
-   
-$sql ="select * from pracownicy,organizacja where id_org=dzial order by imie DESC ";
-echo("<h3>zad.1</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and dzial=3 order by imie ASC ";
-echo("<h3>zad.2</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and imie like'%a' order by imie ASC ";
-echo("<h3>zad.3</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and imie like'%a' and (dzial=1 or dzial=3) order by zarobki ASC ";
-echo("<h3>zad.4</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and imie not like'%a' order by nazwa_dzial ASC, zarobki ASC ";
-echo("<h3>zad.5</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-echo("<h2>Limit</h2>");
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial and dzial=4 limit 2 ";
-echo("<h3>zad.1</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-
-$sql ="select * from pracownicy,organizacja where id_org=dzial and imie like'%a' and (dzial=4 or dzial=2) limit 3 ";
-echo("<h3>zad.2</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    
-$sql ="select * from pracownicy,organizacja where id_org=dzial order by data_urodzenia limit 1 ";
-echo("<h3>zad.3</h3>");
-echo("<h4>$sql</h4>");
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
-}
-echo('</table>');
-    
-    ?>
+    <a href="funkAgregujaca.php">Funkcje agregujące</a>
 </body>
+</html>
+<?php
+    require_once("connect.php");
+
+    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 1</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //---------------------------------------------------------------
+
+    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial AND (dzial = 1 OR dzial = 4)";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 2</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //---------------------------------------------------------------
+
+    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial AND imie like '%a'";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 3</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
+
+    //---------------------------------------------------------------
+
+    $sql = "SELECT * FROM pracownicy, organizacja WHERE id_org = dzial AND imie not like '%a'";
+
+    $result = mysqli_query($conn, $sql);
+
+    echo("<h1>Zadanie 4</h1>");
+    echo("<h2>".$sql."</h2>");
+
+    echo("<table border='1'>");
+    echo("<th>ID</th><th>Imie</th><th>Zarobki</th><th>Data Urodzenia</th><th>Dzial</th><th>Nazwa dzial</th>");
+        while($row = mysqli_fetch_assoc($result)) {
+            echo("<tr>");
+            echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td><td>".$row['dzial']."</td><td>".$row['nazwa_dzial']."</td>");
+            echo("</tr>");
+        };
+    echo("</table>");
 </html>
