95692c0613d7954e3dcc08b3309935ac7ad7b54c
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-11-26 09:10:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 </head>
 <body>
     <h1>Marek Moździerz nr.21</h1>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">github</a>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">•github</a>
     <br>
-    <a href="pracownnicyiorganizacja.php">Organizacja i Pracownicy</a>
-    <a href="funkAgregujaca.php">Funkcje agregujące</a>
-  <a href="DataiCzas.php">Data i Czas</a>
-  <a href="formularz.html">Formularz</a>
-    <a href="danedobazy.html">Danie do bazy</a>
+    <a href="pracownnicyiorganizacja.php">•Organizacja i Pracownicy</a>
+    <a href="funkAgregujaca.php">•Funkcje agregujące</a>
+  <a href="DataiCzas.php">•Data i Czas</a>
+  <a href="formularz.html">•Formularz</a>
+    <a href="danedobazy.html">•Danie do bazy</a>
 </body>
 </html>
 <?php
