070b4927def9375424d7a8cd83623964ead9ac30
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-11-26 08:58:25 +0100

    Update funkAgregujaca.php

--- funkAgregujaca.php
+++ funkAgregujaca.php
@@ -7,7 +7,7 @@
 
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">Github</a>
     <a href="index.php">Pracownicy</a>
-    <a href="pracownicyiorganizacja.php">Organizacja Pracownicy</a>
+    <a href="pracownnicyiorganizacja.php">Organizacja Pracownicy</a>
     <a href="funkAgregujaca.php">Funkcje Agregujace</a>
     
 <?php
