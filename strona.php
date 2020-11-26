4844d73d6710aa92aacdabc275757876c12213ec
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-11-26 09:07:48 +0100

    Create strona.php

---
+++ strona.php
@@ -0,0 +1,13 @@
+<?php
+	echo("Może dziala i zdam ale nie wiem");
+       
+        echo("<ul>");
+
+echo("<li>".$_POST["Firstname"]);
+echo("<li>".$_POST["Lastname"]);
+echo("<li>".$_POST["City"]);
+echo("<li>".$_POST["Phone"]);
+echo("<li>".$_POST["Postcode"]);
+echo("<ul>");
+	
+?>
