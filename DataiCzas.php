dc004f075bd7056b93383d9197b5b74ef6c46cfb
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-11-27 10:10:36 +0100

    Update DataiCzas.php

--- DataiCzas.php
+++ DataiCzas.php
@@ -8,8 +8,8 @@
     <a href="index.php">•Pracownicy</a>
     <a href="pracownnicyiorganizacja.php">•Organizacja Pracownicy</a>
     <a href="funkAgregujaca.php">•Funkcje Agregujace</a>
-    <a href="DataiCzas.php">•Data i Czas</a>
-     <a href="formularz.html">•Formularz</a>
+    <a href="index.php">•strona Główna</a>
+    <a href="formularz.html">•Formularz</a>
     
 
 <?php
