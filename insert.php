c7886fd121236d301da8c1c439801d635c7d2965
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:45:10 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -14,3 +14,14 @@ $conn = new mysqli ($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
+echo("<br>");
+if ($conn->query($sql) === TRUE) {
+  header ('Location:https://https://app-marek.herokuapp.com/danedobazy.html');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
