d49e83c4dc3939b71a3495c0a2b55097e9bb4cf8
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:40:29 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,13 @@
+<?php
+echo("Witaj w insert.php");
+echo "<li>".$_POST['imie'];
+echo "<li>".$_POST['dzial'];
+echo "<li>".$_POST['darobki'];
+echo "<li>".$_POST['data_urodzenia'];
+
+
+$servername = "mysql-mateusz.alwaysdata.net";
+$username = "mateusz";
+$password = "Strona123";
+$dbname = "mateusz_kus";
+
