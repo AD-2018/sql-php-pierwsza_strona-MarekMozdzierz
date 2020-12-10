8f3c67fb3046f4cfbf4cb55f034b8a95c014c517
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:48:20 +0100

    Create DaneDoBazy.php

---
+++ DaneDoBazy.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dodaj pracownika</title>
+  <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Dane do Bazy</title>
+</head>
+<body>
