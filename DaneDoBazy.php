c1cfcae5d2afedf706be7a44b08cec05c7caf13f
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:51:14 +0100

    Update DaneDoBazy.php

--- DaneDoBazy.php
+++ DaneDoBazy.php
@@ -14,3 +14,16 @@
     <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">GitHub</a></li> 
     <li><a href="index.php">Strona Główna</a></li>
 		<li><a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a></li>
+</ul>
+</nav>
+	                Dodawanie Pracownika<br>
+	                <form action="insert.php" method="POST">
+			Imię<br>
+			<input type="text" name="imie"><br>
+			Dział<br>
+			<input type="number" name="dzial"></br>
+			Zarobki<br>
+			<input type="number" name="zarobki"></br>
+			Data Urodzenia<br>
+			<input type="date" name="data_urodzenia"></br>
+			<input type="submit" value="Dodaj Pracownika"><br>
