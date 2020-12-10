cd07cfe6c5b9f818fd2bb0426dcf600bf49774f6
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:50:44 +0100

    Update DaneDoBazy.php

--- DaneDoBazy.php
+++ DaneDoBazy.php
@@ -8,7 +8,7 @@
   <title>Dane do Bazy</title>
 </head>
 <body>
-h1>Marek Moździerz nr.21</h1>
+<h1>Marek Moździerz nr.21</h1>
     <nav>
     <ul>
     <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">GitHub</a></li> 
