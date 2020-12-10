905473ae11b5a49e2209ff4e7c88b4fca2ea0470
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:58:15 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,3 +6,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  header ('Location:https://https://https://app-marek.herokuapp.com/danedobazy.html');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
