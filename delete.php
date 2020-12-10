54928a94c34ff7d250f85b3f647f0695c4df886b
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2020-12-10 08:56:56 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,3 +2,7 @@
 echo $_POST['id_pracownicy'];
 
 require "connect.php";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
