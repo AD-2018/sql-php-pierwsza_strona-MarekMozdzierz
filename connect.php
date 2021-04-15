27e5eb547e74675418b0a1896197b9f65a572278
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2021-04-15 12:24:16 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "mysql-kcz.alwaysdata.net";
-$username = "kcz";
-$password = "zaq1@WSX";
-$dbname = "kcz_20";
+    $servername = "mysql-mateusz.alwaysdata.net";
+    $username = "mateusz";
+    $password = "Strona123";
+    $dbname = "mateusz_kus";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
         die("Connection failed: " .$conn->connect_error);
     };
 ?>
