ef1d98460289ae58a35de4f9045ba0bb349f3010
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2021-04-15 13:35:37 +0200

    Update index.php

--- grid/index.php
+++ grid/index.php
@@ -10,18 +10,18 @@
 <h1>MAREK</h1>
 </div>
     <div class = "nav">
-        <a class="panel" href="/gridcss/grid1/grid.php">grid1</a>
-        <a class="panel" href="/gridcss/grid2/grid.php">grid2</a>
-        <a class="panel" href="/gridcss/grid3/grid.html">grid3</a>
-        <a class="panel" href="/gridcss/grid4/grid.html">grid4</a>
-        <a class="panel" href="/gridcss/grid5/grid.html">grid5</a>
-         <a class="panel" href="/gridcss/grid6/grid.html">grid6</a>
-         <a class="panel" href="/gridcss/grid7/grid.html">grid7</a>
-         <a class="panel" href="/gridcss/grid8/grid.html">grid8</a>
-         <a class="panel" href="/gridcss/grid9/grid.html">grid9</a>
-         <a class="panel" href="/gridcss/grid10/grid.html">grid10</a>
-         <a class="panel" href="/gridcss/grid11/grid.html">grid11</a>
-         <a class="panel" href="/gridcss/grid12/grid.html">grid12</a>
+        <a class="panel" href="/grid/grid1/grid.php">grid1</a>
+        <a class="panel" href="/grid/grid2/grid.php">grid2</a>
+        <a class="panel" href="/grid/grid3/grid.html">grid3</a>
+        <a class="panel" href="/grid/grid4/grid.html">grid4</a>
+        <a class="panel" href="/grid/grid5/grid.html">grid5</a>
+         <a class="panel" href="/grid/grid6/grid.html">grid6</a>
+         <a class="panel" href="/grid/grid7/grid.html">grid7</a>
+         <a class="panel" href="/grid/grid8/grid.html">grid8</a>
+         <a class="panel" href="/grid/grid9/grid.html">grid9</a>
+         <a class="panel" href="/grid/grid10/grid.html">grid10</a>
+         <a class="panel" href="/grid/grid11/grid.html">grid11</a>
+         <a class="panel" href="/grid/grid12/grid.html">grid12</a>
         
     </div>
     </body>
