c1ec68736a385d60e9728e58c2822b9a776127b2
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2021-04-15 12:50:34 +0200

    Update index.php

--- grid/index.php
+++ grid/index.php
@@ -1,1 +1,28 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="/style.css">
+<title>GRID</title>
+</head>
+<body>
+<div class= "imie">    
+<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-MarekMozdzierz">GitHub</a>
+<h1>MAREK</h1>
+</div>
+    <div class = "nav">
+        <a class="panel" href="/gridcss/grid1/grid.php">grid1</a>
+        <a class="panel" href="/gridcss/grid2/grid.php">grid2</a>
+        <a class="panel" href="/gridcss/grid3/grid.html">grid3</a>
+        <a class="panel" href="/gridcss/grid4/grid.html">grid4</a>
+        <a class="panel" href="/gridcss/grid5/grid.html">grid5</a>
+         <a class="panel" href="/gridcss/grid6/grid.html">grid6</a>
+         <a class="panel" href="/gridcss/grid7/grid.html">grid7</a>
+         <a class="panel" href="/gridcss/grid8/grid.html">grid8</a>
+         <a class="panel" href="/gridcss/grid9/grid.html">grid9</a>
+         <a class="panel" href="/gridcss/grid10/grid.html">grid10</a>
+         <a class="panel" href="/gridcss/grid11/grid.html">grid11</a>
+         <a class="panel" href="/gridcss/grid12/grid.html">grid12</a>
+        
+    </div>
+    </body>
+    </html>
