6a146ce0ef84695f2c7d03c2340fa38b0e1ee18d
Marek Moździerz <73634433+MarekMozdzierz@users.noreply.github.com>
2021-04-22 09:01:58 +0200

    Update usunnau.php

--- grid/grid1/usunnau.php
+++ grid/grid1/usunnau.php
@@ -1,1 +1,26 @@
+ <?php
+echo("Usuń");
+echo $_POST['ID'];
 
+
+require_once("connect.php");
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM Nauczyciele WHERE id_naucz=".$_POST['id_naucz'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+    header ('Location:https://app-marek.herokuapp.com/grid/grid1/grid.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
